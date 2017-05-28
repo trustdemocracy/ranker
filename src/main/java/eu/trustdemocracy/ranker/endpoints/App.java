40c54070dc833cc44e495967bfd77225fd001075
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-05-28 02:54:09 +0200

    Improve App entrypoint

--- src/main/java/eu/trustdemocracy/ranker/endpoints/App.java
+++ src/main/java/eu/trustdemocracy/ranker/endpoints/App.java
@@ -1,23 +1,45 @@
 package eu.trustdemocracy.ranker.endpoints;
 
+import eu.trustdemocracy.ranker.endpoints.util.Runner;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
+import lombok.val;
 
 public class App extends AbstractVerticle {
 
-    @Override
-    public void start() {
-        Router router = Router.router(vertx);
-        router.route().handler(BodyHandler.create());
-        router.get("/").handler(this::handleProposals);
+  private static final Logger LOG = LoggerFactory.getLogger(App.class);
+  private static final int DEFAULT_PORT = 8080;
 
-        vertx.createHttpServer().requestHandler(router::accept).listen(8080);
-    }
+  private Router router;
 
-    private void handleProposals(RoutingContext routingContext) {
-        routingContext.response().putHeader("content-type", "application/json").end("{'status': 'ok'}");
-    }
+  public static void main(String... args) {
+    Runner.runVerticle(App.class.getName());
+  }
+
+  @Override
+  public void start() throws Exception {
+    val port = config().getInteger("http.port", DEFAULT_PORT);
+
+    vertx.executeBlocking(future -> {
+      router = Router.router(vertx);
+      router.route().handler(BodyHandler.create());
+
+      vertx.createHttpServer()
+          .requestHandler(router::accept)
+          .listen(port);
+
+      future.complete();
+    }, result -> {
+      if (result.succeeded()) {
+        LOG.info("App listening on port: " + port);
+
+      } else {
+        LOG.error("Failed to start verticle", result.cause());
+      }
+    });
+  }
 
 }
