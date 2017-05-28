1d8d816f7033b7130d2ac8ce6dc8af32a9c82fd7
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-05-28 06:12:11 +0200

    Add getGraph failing test

--- src/test/java/eu/trustdemocracy/ranker/gateways/repositories/mongo/MongoRankRepositoryTest.java
+++ src/test/java/eu/trustdemocracy/ranker/gateways/repositories/mongo/MongoRankRepositoryTest.java
@@ -4,6 +4,7 @@ import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.github.fakemongo.Fongo;
 import com.mongodb.client.MongoDatabase;
@@ -144,5 +145,51 @@ public class MongoRankRepositoryTest {
     rankRepository.removeUser(user);
     assertEquals(0L, usersCollection.count());
     assertEquals(0L, relationshipsCollection.count());
+  }
+
+  @Test
+  public void getGraph() {
+    val user1 = new User().setId(UUID.randomUUID());
+    val user2 = new User().setId(UUID.randomUUID());
+    val user3 = new User().setId(UUID.randomUUID());
+    val user4 = new User().setId(UUID.randomUUID());
+
+    rankRepository.createUser(user1);
+    rankRepository.createUser(user2);
+    rankRepository.createUser(user3);
+    rankRepository.createUser(user4);
+
+    val relationship1 = new Relationship().setOriginId(user1.getId()).setTargetId(user2.getId());
+    val relationship2 = new Relationship().setOriginId(user2.getId()).setTargetId(user3.getId());
+    val relationship3 = new Relationship().setOriginId(user1.getId()).setTargetId(user4.getId());
+    val relationship4 = new Relationship().setOriginId(user2.getId()).setTargetId(user4.getId());
+    val relationship5 = new Relationship().setOriginId(user3.getId()).setTargetId(user4.getId());
+    val relationship6 = new Relationship().setOriginId(user1.getId()).setTargetId(user3.getId());
+    val relationship7 = new Relationship().setOriginId(user2.getId()).setTargetId(user1.getId());
+
+    rankRepository.createRelationship(relationship1);
+    rankRepository.createRelationship(relationship2);
+    rankRepository.createRelationship(relationship3);
+    rankRepository.createRelationship(relationship4);
+    rankRepository.createRelationship(relationship5);
+    rankRepository.createRelationship(relationship6);
+    rankRepository.createRelationship(relationship7);
+
+    val graph = rankRepository.getGraph();
+
+    assertEquals(3, graph.get(user1.getId()).getOutRelationshipsCount());
+    assertTrue(graph.get(user1.getId()).getInRelationships().contains(user2.getId()));
+
+    assertEquals(3, graph.get(user2.getId()).getOutRelationshipsCount());
+    assertTrue(graph.get(user2.getId()).getInRelationships().contains(user1.getId()));
+
+    assertEquals(1, graph.get(user3.getId()).getOutRelationshipsCount());
+    assertTrue(graph.get(user3.getId()).getInRelationships().contains(user1.getId()));
+    assertTrue(graph.get(user3.getId()).getInRelationships().contains(user2.getId()));
+
+    assertEquals(0, graph.get(user4.getId()).getOutRelationshipsCount());
+    assertTrue(graph.get(user4.getId()).getInRelationships().contains(user1.getId()));
+    assertTrue(graph.get(user4.getId()).getInRelationships().contains(user2.getId()));
+    assertTrue(graph.get(user4.getId()).getInRelationships().contains(user3.getId()));
   }
 }
